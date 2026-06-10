165d2ce4736aab78168a38d0b91de6420503cbe7
punjabone12345 <punjabone12345@gmail.com>
2026-06-10 08:19:28 +0530

    Change OANDA token and account number
    Updated OANDA API token and account number.

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
-const OANDA_TOKEN = "16cb94bd836cdf997245ab4e665dfc27-e40ff75e4aefa3dca78ca044e833995c";
-const OANDA_ACCT = "101-001-38845298-001";
+const OANDA_TOKEN = "e7c99118529d3a66dc1a701abe3c57d3-93b27b741d06960a4a380cc71c84d275";
+const OANDA_ACCT = "101-001-38845298-002";
 const OANDA_BASE = "https://api-fxpractice.oanda.com/v3";
 
 export default {
