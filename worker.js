ed7d05fe7d45bcd3fcc4cb700141095015ec72e6
punjabone12345 <punjabone12345@gmail.com>
2026-06-10 08:32:02 +0530

    Change OANDA_BASE URL to use practice API

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 const OANDA_TOKEN = "e7c99118529d3a66dc1a701abe3c57d3-93b27b741d06960a4a380cc71c84d275";
 const OANDA_ACCT = "101-001-38845298-002";
-const OANDA_BASE = "https://api-fxtrade.oanda.com/v3";
+const OANDA_BASE = "https://api-fxpractice.oanda.com/v3";
 export default {
   async fetch(req) {
     if (req.method === "OPTIONS") {
